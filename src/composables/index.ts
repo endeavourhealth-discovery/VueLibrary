b985f0003d304db6710d72f3fcd9a45db324cff2
matt-voror <matt@voror.co.uk>
2026-03-27 16:38:47 +0000

    removed IMDirectory only composables

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,29 +1,9 @@
-import { useAutocompleteRegistry } from "./useAutocompleteRegistry";
 import { useChangeFontSize } from "./useChangeFontSize";
 import { useChangeThemeOptions } from "./useChangeThemeOptions";
 import { useCopyToClipboard } from "./useCopyToClipboard";
-import { useCreateNew } from "./useCreateNew";
 import { useDownloadFile } from "./useDownloadFile";
-import { useEclValidator } from "./useEclValidator";
 import { useOverlay } from "./useOverlay";
 import { useSpeechToText } from "./useSpeechToText";
-import { useTerms } from "./useTerms";
 import { useTree } from "./useTree";
-import { useValidity } from "./useValidity";
-import { useValueVariableMap } from "./useValueVariableMap";
 
-export {
-  useAutocompleteRegistry,
-  useChangeFontSize,
-  useChangeThemeOptions,
-  useCopyToClipboard,
-  useCreateNew,
-  useDownloadFile,
-  useEclValidator,
-  useOverlay,
-  useSpeechToText,
-  useTerms,
-  useTree,
-  useValidity,
-  useValueVariableMap
-};
+export { useChangeFontSize, useChangeThemeOptions, useCopyToClipboard, useDownloadFile, useOverlay, useSpeechToText, useTree };
