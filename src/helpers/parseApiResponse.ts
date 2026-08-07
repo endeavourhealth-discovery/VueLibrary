f1d438e71c0589265106503ffc37f4dc031110f4
matt-voror <matt@voror.co.uk>
2026-08-07 20:53:48 +0100

    parseApiResponse no longer multi functional for arrays. Default is standard parse.

--- src/helpers/parseApiResponse.ts
+++ src/helpers/parseApiResponse.ts
@@ -1,22 +1,14 @@
 import z from "zod";
 
-export function parseApiResponse<T>(data: unknown, schema: z.ZodType<T>): T;
-
-export function parseApiResponse<T>(data: unknown, schema: z.ZodType<T>, isArrayOf: true): T[];
-
-export function parseApiResponse<T>(data: unknown, schema: z.ZodType<T>, isArrayOf = false): T | T[] {
-  const parser = isArrayOf ? z.array(schema) : schema;
-
-  if (import.meta.env.VITE_SAFE_PARSE_API_RESPONSE === "true") {
-    const parsed = parser.safeParse(data);
-
+export function parseApiResponse<T>(data: unknown, schema: z.ZodType<T>): T {
+  if (import.meta.env.VITE_SAFE_PARSE_API_RESPONSE !== "true") {
+    return schema.parse(data);
+  } else {
+    const parsed = schema.safeParse(data);
     if (parsed.success) {
       return parsed.data;
     }
-
     console.error(parsed.error);
-    return data as T | T[];
+    return data as T;
   }
-
-  return parser.parse(data);
 }
